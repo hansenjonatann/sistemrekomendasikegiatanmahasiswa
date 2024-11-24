3814a20a99b75bc96e5f05595af691c43a83d49b
hansenjonatann <hansenjonatann@gmail.com>
2024-11-24 09:50:07 +0700

    fix error library never used'

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { ArrowLeft, ArrowLeftCircle, HomeIcon, Info, User } from "lucide-react";
+import { HomeIcon, Info, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
